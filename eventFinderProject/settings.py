a5d4d0b5baffd5290208317052bc9ff325fc34a4
sherikopec <sherikopec@gmail.com>
2019-09-07 09:18:43 +0800

    update version for git errors

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'freeeventfinder', # you will need to create this db
-        'USER': '', # enter your user name here
-        'PASSWORD': '',
+        'USER': 'sheridan', # enter your user name here
+        'PASSWORD': 'django9092',
         'HOST': 'localhost',
         'PORT': '5432',
     }
