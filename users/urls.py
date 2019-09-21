af3f15f30426fd6d93c21b8cb348d4bc601e65ad
sherikopec <sherikopec@gmail.com>
2019-09-21 10:36:15 +0800

    fixed user change form

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
+    path('editprofile/', views.EditProfile.as_view(), name = 'editprofile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class Register(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+
+class EditProfile(generic.UpdateView):
+    form_class = CustomUserChangeForm
+    success_url = reverse_lazy('eventFinderApp:account')
+    template_name = 'registration/editprofile.html'
+
+    #get object
+    def get_object(self, queryset=None): 
+        return self.request.user
